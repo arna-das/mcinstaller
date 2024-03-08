6ada6ec97c5fd066556b3d38ce8e5d49ff28020b
arnadas <142620137+arna-das@users.noreply.github.com>
2024-03-08 14:13:21 +0530

    Create sureint.sh

---
+++ sureint.sh
@@ -0,0 +1,17 @@
+rm test.sh
+rm start.sh
+rm jdk.sh
+rm jdk1.sh
+rm installer.sh
+rm -r software
+rm playit.sh
+rm viaversion.sh
+rm viabackword.sh
+rm downloadplug.sh
+rm home.sh
+rm ram.sh
+rm on.sh
+rm on1.sh
+
+
+./home.sh
