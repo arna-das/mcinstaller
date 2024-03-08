2366991252f7125bc047b1e92fcd27c387af1ba8
arnadas <142620137+arna-das@users.noreply.github.com>
2024-03-08 14:09:48 +0530

    Create home.sh

---
+++ home.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+clear
+echo "################################################################################"
+echo "#"                                                                
+echo "#                                 Not_arnadas"                         
+echo "#"                                                                      
+echo "#                         Copyright (C) 2023 - 2024"                 
+echo "#"                                                                      
+echo "#"                                                                    
+echo "################################################################################"
+echo ""
+echo ""
+echo "* [0] start the server"
+echo "* [1] update the ram"
+echo "* [2] download plugins"
+echo "* [3] update the repo"
+echo "* [4] dengeour zone"
+
+ 
+read -p " (0-4): " input
+
+case $input in
+    0)
+    ./on.sh
+
+    ;;
+
+    1)
+    ./ram.sh
+        ;;
+
+    2)
+    ./downloadplug.sh
+        ;;
+
+    3)
+    ./viabackword
+        ;;
+
+    4)
+    bash dangeour.sh
+        ;;
+
+     
